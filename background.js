af42ff7085c7f10e8f520d99c762606e6a9bfb00
Anush Serrao <60592571+ashserrao@users.noreply.github.com>
2024-01-31 16:36:36 +0530

    devtools actions

--- background.js
+++ background.js
@@ -1,17 +1,72 @@
+let cId = 196;
+let clientUrls = ["chrome://","examroom.ai"];
+
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.tabs.create({
         url:'https://www.examroom.ai'
     }),
-    chrome.tabs.query({ active:true, currentWindow:true}, function(activeTab) {
-        const currentTabId = activeTab[0].id;
-
-        chrome.tabs.query({ currentWindow:true}, function(allTabs) {
-            allTabs.forEach(function(tab) {
-                if(tab.id !== currentTabId){
-                    chrome.tabs.remove(tab.id);
-                }
-            });
+    chrome.tabs.query({currentWindow: true}, function(allTabs) {
+        allTabs.forEach(function(tab) {
+            const tabUrl = tab.url;
+            let isMatched = clientUrls.some(allowedurl => tabUrl.includes(allowedurl));
+            if(!isMatched){
+                chrome.tabs.remove(tab.id);
+            }
         })
     })
 });
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if(message === 'app-data'){
+        fetch('https://examroom.ai/candidate/assets/script/allowedurl1.json')
+        .then(response => {
+            if(!response.ok){
+                throw new Error('Data failed from network side')
+            }
+            return response.json();
+        })
+        .then( data => {
+            const clientData = data.key.find(element => element.clientId === cId);
+            if(clientData){
+                const allowedUrls = clientData.allowedurls;
+                console.log('data-success');
+                clientUrls = allowedUrls;
+                sendResponse(allowedUrls);
+            } else {
+                sendResponse('client urls not found')
+            }
+        })
+        .catch(error => {
+            console.error('error fetching data', error)
+        })
+        return true
+    }
+});
+
+chrome.tabs.onUpdated.addListener(() => {
+    chrome.tabs.query({currentWindow: true}, function(allTabs) {
+        allTabs.forEach(function(tab) {
+            const tabUrl = tab.url;
+            let isMatched = clientUrls.some(allowedurl => tabUrl.includes(allowedurl));
+            if(!isMatched){
+                chrome.tabs.remove(tab.id);
+            }
+        });
+    });
+});
+
+
+chrome.runtime.onConnect.addListener(port => {
+    if (port.name === "devtools") {
+      port.onMessage.addListener(msg => {
+        if (msg.name === "openDevTools") {
+            chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+                const currentTab = tabs[0];
+                chrome.tabs.remove(currentTab.id);
+            });
+        }
+      });
+    }
+  });  
+
--- cont-cont.js
+++ cont-cont.js
@@ -1,5 +1,5 @@
-let spaceCount = 0; 
 const body = document.querySelector('body');
+let spaceCount = 0;
 
 const blockContent = () => {
     body.style.opacity = '0';
@@ -9,22 +9,24 @@ const unBlockContent = () => {
     body.style.opacity = '1';
 }
 
+
+// 1. Send a message to the background script or content script requesting the user's data
+chrome.runtime.sendMessage('api-data', (response) => {
+    console.log('Received API data:', response);
+});
+
+
 document.addEventListener('keydown', function (e) {
-    if(e.ctrlKey){
-        console.log('Content blocked due to ctrl keys');
+    if (e.ctrlKey) {
+        console.log("KeyStroke", e);
         blockContent();
     } else if(e.key === " ") {
         spaceCount++;
         if(spaceCount === 2){
-            // console.log(spaceCount);
             unBlockContent();
             spaceCount = 0;
         }
     } else {
-        console.log("Keydown event failed", e);
+        console.log('OnKey down failed', e);
     }
-})
-
-chrome.runtime.sendMessage('app-data', response => {
-    console.log(response);
-})
+});
